191b2f6d114caba91c61b8a4299c7c1647914706
Ray Grillo Fujiyoshi <111706736+RayGrillo@users.noreply.github.com>
2024-10-07 09:53:22 -0300

    Add files via upload

---
+++ graficos/usuarios.js
@@ -0,0 +1,59 @@
+import { getCSS, tickConfig } from "./common.js";
+
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--cor-segundaria')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-cor'),
+        paper_bgcolor: getCSS('--bg-cor'),
+        title: {
+            text: 'Redes sociais com mais usuários no mundo',
+            x: 0,
+            font: {
+                color: getCSS('--cor-principal'),
+                family: getCSS('--fonte'),
+                size: 27
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Nome das redes sociais',
+                font: {
+                    color: getCSS('--cor-segundaria')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--cor-segundaria')
+                }
+            }
+    }
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+}
+
+quantidadeUsuarios()
